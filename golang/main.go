5dad1afacd893d4a374f6e6d701409dc438cc5cd
Jamaludin <74maludin@gmail.com>
2023-10-30 13:58:33 +0700

    add gin framework

--- golang/main.go
+++ golang/main.go
@@ -1,34 +1,50 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type HelloWorld struct {
 	Hello string
 }
 
-func handlerIndex(w http.ResponseWriter, r *http.Request) {
+func setupRoute() *gin.Engine {
 	var message = HelloWorld{
 		Hello: "World",
 	}
 
-	b, err := json.Marshal(message)
-	if err != nil {
-		fmt.Printf("Error: %s", err)
-	}
+	r := gin.Default()
 
-	w.Write(b)
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, message.Hello)
+	})
+
+	return r
 }
 
-func main() {
-	http.HandleFunc("/", handlerIndex)
+// func handlerIndex(w http.ResponseWriter, r *http.Request) {
+// 	var message = HelloWorld{
+// 		Hello: "World",
+// 	}
 
-	var address = "localhost:3001"
-	err := http.ListenAndServe(address, nil)
-	if err != nil {
-		fmt.Printf("Error: %s", err)
-	}
+// 	b, err := json.Marshal(message)
+// 	if err != nil {
+// 		fmt.Printf("Error: %s", err)
+// 	}
+
+// 	w.Write(b)
+// }
+
+func main() {
+	// http.HandleFunc("/", handlerIndex)
+
+	// var address = "localhost:3001"
+	// err := http.ListenAndServe(address, nil)
+	// if err != nil {
+	// 	fmt.Printf("Error: %s", err)
+	// }
+	r := setupRoute()
+	r.Run(":3001")
 }
