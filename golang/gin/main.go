0529c51bb470bd6f30889ed703255d8e44eaa797
Jamaludin <74maludin@gmail.com>
2024-03-11 09:40:55 +0700

    update readme, golang

--- golang/gin/main.go
+++ golang/gin/main.go
@@ -6,45 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type HelloWorld struct {
-	Hello string
-}
-
-func setupRoute() *gin.Engine {
-	var message = HelloWorld{
-		Hello: "World",
-	}
-
+func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
 	r.GET("/", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, message.Hello)
+		ctx.JSON(http.StatusOK, gin.H{"data": "Hello World"})
 	})
 
-	return r
-}
-
-// func handlerIndex(w http.ResponseWriter, r *http.Request) {
-// 	var message = HelloWorld{
-// 		Hello: "World",
-// 	}
-
-// 	b, err := json.Marshal(message)
-// 	if err != nil {
-// 		fmt.Printf("Error: %s", err)
-// 	}
-
-// 	w.Write(b)
-// }
-
-func main() {
-	// http.HandleFunc("/", handlerIndex)
-
-	// var address = "localhost:3001"
-	// err := http.ListenAndServe(address, nil)
-	// if err != nil {
-	// 	fmt.Printf("Error: %s", err)
-	// }
-	r := setupRoute()
 	r.Run(":3001")
 }
